4235dcada840294423eafacc4976e2d079d99b8c
Marcus Rådell <marcus@radell.net>
2025-11-11 10:12:54 +0100

    refactor profile part of the CV into a component

---
+++ src/profile.tsx
@@ -0,0 +1,16 @@
+export function Profile() {
+  return (
+    <header>
+      <div className="container">
+        <div className="profile-header">
+          <div className="profile-info">
+            <h1 id="profile-name"></h1>
+            <div>
+              <ul className="expertise-list" id="expertise-list"></ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
