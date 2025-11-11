e67a41e1f339f41e23731baf5483cf575044e122
Marcus Rådell <marcus@radell.net>
2025-11-11 11:45:18 +0100

    add back about section

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Profile } from "./profile";
+import { About } from "./about";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Profile />
+
+    <main className="container">
+      <About />
+    </main>
   </StrictMode>,
 );
