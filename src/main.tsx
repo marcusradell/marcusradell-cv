73fe8762748bea34bc8bd1658fed3ccc4ebad6f0
Marcus Rådell <marcus@radell.net>
2025-11-11 11:56:17 +0100

    break out container component

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
-import { StrictMode, type ReactNode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Profile } from "./profile";
 import { About } from "./about";
-
-type Props = { children: ReactNode };
-
-function Container({ children }: Props) {
-  return <div className="container">{children}</div>;
-}
+import { Container } from "./container";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
